22153d339b049002a96ff0c09edacee7c1efb2f1
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-03-24 19:30:30 +0100

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -11,7 +11,7 @@ export default function GlobalError({
     // global-error must include html and body tags
     <html>
       <body>
-        <h2>Something went wrong!</h2>
+        <h2>Something went wrong!</h2> <span>{error.message}</span>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
