cdc7e777f65f54e66ddccfc62d0176cfe7364081
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-03-27 20:55:56 +0100

    added spinner

--- src/components/WayForPay/WayForPayButton.tsx
+++ src/components/WayForPay/WayForPayButton.tsx
@@ -1,27 +1,46 @@
 "use client";
 
+import { useState } from "react";
 import ButtonYelow from "../common/ButtonYelow";
 import { getForm } from "./operation";
+import LoadingSpiner from "../common/LoadingSpiner";
 
 export default function WayForPayButton({ className }: { className?: string }) {
+  const [isLoader, setIsLoder] = useState(false);
   const handleGoToPay = async () => {
-    const html = await getForm();
+    setIsLoder(true);
+    try {
+      const html = await getForm();
 
-    let container = document.getElementById("wayforpay-container");
-    if (!container) {
-      container = document.createElement("div");
-      container.id = "wayforpay-container";
-      document.body.appendChild(container);
+      let container = document.getElementById("wayforpay-container");
+      if (!container) {
+        container = document.createElement("div");
+        container.id = "wayforpay-container";
+        document.body.appendChild(container);
+      }
+
+      container.innerHTML = html;
+
+      const form = document.getElementById("wayforpayForm") as HTMLFormElement;
+      if (form) form.submit();
+      else {
+        console.error("Форма не знайдена");
+        setIsLoder(false);
+      }
+    } catch (error) {
+      console.log("Помилка при оплаті");
+      setIsLoder(false);
     }
-
-    container.innerHTML = html;
-
-    const form = document.getElementById("wayforpayForm") as HTMLFormElement;
-    if (form) form.submit();
-    else console.error("Форма не знайдена");
   };
 
   return (
-    <ButtonYelow onClick={handleGoToPay} title="Купити" className={className} />
+    <>
+      {isLoader && <LoadingSpiner />}
+      <ButtonYelow
+        onClick={handleGoToPay}
+        title="Купити"
+        className={className}
+      />
+    </>
   );
 }
--- src/components/common/LoadingSpiner.tsx
+++ src/components/common/LoadingSpiner.tsx
@@ -1,6 +1,6 @@
 function LoadingSpiner() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-black/50">
+    <div className="fixed top-0 right-0 left-0 flex h-screen w-full items-center justify-center bg-black/50">
       <span className="loader"></span>
     </div>
   );
