8a5e33a575e9a787e117a3ecb5f97ca3fcca0c13
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-04-16 17:57:41 +0200

    new img

--- src/components/common/sections/SectionPayCard/SectionCertificate.tsx
+++ src/components/common/sections/SectionPayCard/SectionCertificate.tsx
@@ -12,20 +12,39 @@ function SectionCertificate() {
       <Container>
         <SubtitlePoint text="подаруй враження" className="mb-6" />
         <div className="xl:flex xl:items-start xl:justify-between">
-          <Image
+          <div className="relative top-0 mx-auto h-[480px] w-full xl:sticky">
+            <Image
+              className="absolute bottom-0 left-1/2 -translate-x-1/2"
+              width={429}
+              height={303}
+              alt="Подарунковий сертифікат"
+              // src={"/images/certificato.webp"}
+              src={"/images/certificato.jpg"}
+            />
+            <Image
+              className="absolute bottom-32 left-[1%] z-10 -rotate-12 xl:left-[8%]"
+              width={429}
+              height={303}
+              alt="Подарунковий сертифікат"
+              // src={"/images/certificato.webp"}
+              src={"/images/certificato.jpg"}
+            />
+          </div>
+
+          {/* <Image
             className="top-0 mb-20 hidden md:block xl:sticky xl:mb-0 xl:max-w-[588px]"
             src={"/images/pay_card.png"}
             width={888}
             height={600}
             alt="Подарунковий сертифікат"
-          />
-          <Image
+          /> */}
+          {/* <Image
             className="mx-auto mb-20 md:hidden"
             src={"/images/pay_card_mobile.png"}
             width={728}
             height={478}
             alt="Подарунковий сертифікат"
-          />
+          /> */}
           <div className="xl:max-w-[488px]">
             <TitleSection title="Ваш ідеальний подарунок! " className="mb-7" />
             <ul className="flex w-full flex-col gap-3">
