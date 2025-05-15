f035494cc2e48bb73d07460156ef037e7f1b81d5
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-05-15 20:12:29 +0200

    fixed position video

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
         <Toaster richColors />
         <Analytics />
 
-        <div className="fixed right-4 bottom-0 z-[10000] md:right-10">
+        <div className="fixed right-4 bottom-[34px] z-[10000] md:right-10">
           <VideoIntroduction />
           <div className="flex justify-end">
             <ButtonScrollToTop />
