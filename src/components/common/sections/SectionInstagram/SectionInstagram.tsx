a165216732bbb005f3a325a1db8cadcbec6d23b7
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-05-20 18:38:55 +0200

    new photo instagram1

--- src/components/common/sections/SectionInstagram/SectionInstagram.tsx
+++ src/components/common/sections/SectionInstagram/SectionInstagram.tsx
@@ -1,7 +1,6 @@
 import Container from "../../Container";
 import TitleSection from "../../TitleSection";
 import InstagramLink from "../../Links/InstagramLink";
-import Image from "next/image";
 
 function SectionInstagram() {
   return (
@@ -10,34 +9,34 @@ function SectionInstagram() {
         <TitleSection title="Наш інстаграм" />
         <InstagramLink isIcon={false} />
         <ul className="mt-2.5 flex justify-around gap-5">
-          <li className="relative w-fit">
-            <Image
-              src={"/images/Instagram_1.jpg"}
-              width={488}
-              height={560}
-              className="rounded-3xl"
-              alt="Зображення із інстаграм"
-            />
+          <li
+            style={{
+              background: "url('/images/Instagram_1.PNG')",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+            className="relative h-[700px] w-[488px] overflow-hidden rounded-3xl md:max-h-[578px] xl:max-h-[458px]"
+          >
             <InstagramLink className="absolute right-4 bottom-4" />
           </li>
-          <li className="relative hidden w-fit md:block">
-            <Image
-              src={"/images/Instagram_2.jpg"}
-              width={488}
-              height={560}
-              className="rounded-3xl"
-              alt="Зображення із інстаграм"
-            />
+          <li
+            style={{
+              background: "url('/images/Instagram_2.jpg')",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+            className="relative hidden h-[700px] w-[488px] overflow-hidden rounded-3xl md:block md:max-h-[578px] xl:max-h-[458px]"
+          >
             <InstagramLink className="absolute right-4 bottom-4" />
           </li>
-          <li className="relative hidden w-fit xl:block">
-            <Image
-              src={"/images/Instagram_3.jpg"}
-              width={488}
-              height={560}
-              className="rounded-3xl"
-              alt="Зображення із інстаграм"
-            />
+          <li
+            style={{
+              background: "url('/images/Instagram_3.jpg')",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+            className="relative hidden h-[700px] w-[488px] overflow-hidden rounded-3xl md:max-h-[578px] xl:block xl:max-h-[458px]"
+          >
             <InstagramLink className="absolute right-4 bottom-4" />
           </li>
         </ul>
